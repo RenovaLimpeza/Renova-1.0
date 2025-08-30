8f862fdfaee36e6860a1034fc9169dab7b033182
Rhyan Komm <rhyankomm@hotmail.com>
2025-08-29 21:16:06 -0400

    fix: arrumando icones

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export type { NavLink } from "./Navbar";
 export { default as Hero } from "./Hero";
 export { default as WhatsAppButton } from "./WhatsAppButton";
 export { default as AboutSection } from "./AboutSection";
+export { default as ServicesSection } from "./ServicesSection";
