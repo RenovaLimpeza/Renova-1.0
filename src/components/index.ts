7bb410a10c7865c82e0cb1682a2d93a781c70bdb
Rhyan Komm <rhyankomm@hotmail.com>
2025-08-29 22:44:55 -0400

    feat: galeria de serviços

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Hero } from "./Hero";
 export { default as WhatsAppButton } from "./WhatsAppButton";
 export { default as AboutSection } from "./AboutSection";
 export { default as ServicesSection } from "./ServicesSection";
+export { default as ServiceGallerySection } from "./ServiceGallerySection";
